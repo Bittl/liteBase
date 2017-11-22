62ceed998a4f5a53efc62472ea42483f32040122
Bittl <bittl100@gmail.com>
2017-11-22 10:57:28 +0300

    Доработал выбор БД

--- src/createDB/SelestDB.java
+++ src/createDB/SelestDB.java
@@ -2,6 +2,8 @@ package createDB;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SelestDB extends JDialog {
 
@@ -9,11 +11,11 @@ public class SelestDB extends JDialog {
 
         super(frame, "Базы данных");
         setSize(300, 300);
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridLayout(3, 1));
 
 
         JPanel createPanel = new JPanel();
-        createPanel.setLayout(new GridLayout(4, 1));
+        createPanel.setLayout(new BoxLayout(createPanel, BoxLayout.Y_AXIS));
         JLabel createPanelLabel = new JLabel("Создать БД");
         JTextField createField = new JTextField(100);
         JButton createButton = new JButton("Создать");
@@ -21,10 +23,10 @@ public class SelestDB extends JDialog {
         createPanel.add(createField);
         createPanel.add(createButton);
         add(createPanel);
-        add(new JSeparator());
+//        add(new JSeparator());
 
         JPanel openPanel = new JPanel();
-        openPanel.setLayout(new GridLayout(3, 2));
+        openPanel.setLayout(new BoxLayout(openPanel, BoxLayout.Y_AXIS));
         JLabel openLabel = new JLabel("Открыть БД");
         JTextField openField = new JTextField(100);
         JButton openPath = new JButton("поиск");
@@ -33,18 +35,46 @@ public class SelestDB extends JDialog {
         openPanel.add(openField);
         openPanel.add(openPath);
         add(openPanel);
-        add(new JSeparator());
+//        add(new JSeparator());
 
         JButton okButton = new JButton("OK");
         JButton cancelButton = new JButton("Cancel");
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(1, 2));
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
         buttonPanel.add(okButton);
         buttonPanel.add(cancelButton);
 
         add(buttonPanel);
 
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        final JDialog[] sdb = {null};
+
+
+
+        JButton button = new JButton("button");
+        button.setSize(20, 20);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (sdb[0] == null){
+                    sdb[0] = new SelestDB(frame);
+                }
+                sdb[0].setVisible(true);
+            }
+        });
+
+        frame.add(button);
+
+
+        frame.setVisible(true);
 
     }
 }
